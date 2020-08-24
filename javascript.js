33f4630b3b8749ebd6933dd6fdbee8b546cbeafd
Jodie Wiggins <jwiggi@LSW211-JoWigg.attlocal.net>
2020-08-24 15:03:04 -0500

    blog randomizer

---
+++ javascript.js
@@ -0,0 +1,27 @@
+var blogs = [
+  "https://evolutionsisfun.blogspot.com/",
+  "https://aczapkay.blogspot.com",
+  "https://evolutionwithdelaney.blogspot.com/",
+"https://aaronsfaulk.blogspot.com/",
+"https://morgandyh.blogspot.com/",
+"https://evolutionf20al.blogspot.com/",
+"https://johnathanmartinosu.blogspot.com/",
+"https://abbimeek22.blogspot.com/",
+"https://kaelynpage.blogspot.com/",
+"https://catalinaperezzz.blogspot.com/",
+"https://emmaphilbin.blogspot.com/",
+"https://morgankkate.blogspot.com",
+"https://jsisov.blogspot.com",
+"https://laurenoliviasuggs.blogspot.com",
+"https://aubreevanmeter.blogspot.com",
+"https://cassidyvaughnevolution.blogspot.com/",
+"Not set up yet - click again",
+"https://evolution-hayley.blogspot.com/",
+"https://pkwevolution.blogspot.com",
+"Not set up yet - click again"
+]
+
+function newBlog() {
+  var randomNumber = Math.floor(Math.random() * (blogs.length));
+  document.getElementById('blogDisplay').innerHTML = blogs[randomNumber];
+}
