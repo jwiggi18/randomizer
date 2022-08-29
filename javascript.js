8326aea9413b9633cb6a54c7a880b00b254e2c26
jwiggi18 <jodiewiggins18@gmail.com>
2022-08-29 13:18:44 -0500

    updated addresses

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 var blogs = [
-"https://brobowe.blogspot.com/", 
-"https://shanesevolutionarysoup.blogspot.com",
-"https://evolutionbytaylor.blogspot.com",
-"https://starfilled57evolution.blogspot.com",
-"https://brandonneat.blogspot.com",
-"https://thinkspeakrepeat.blogspot.com"
-
+"https://ameyali12365.blogspot.com/", 
+"http://belahadleyosu.blogspot.com/",
+"https://azizelias339.blogspot.com/",
+"https://graciesblog76.blogspot.com/",
+"https://haneenabouhassounblogforevolution.blogspot.com",
+"https://evolutionodance.blogspot.com/",
+"https://evolutionbymarie.blogspot.com",
+"https://premedmel.blogspot.com/",
+"https://nateokstate.blogspot.com/"
 
 
 ]
