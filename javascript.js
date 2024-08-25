4c18c98ffeb7e9a536791ae4e5dca0f1a7d651e0
jwiggi18 <jodiewiggins18@gmail.com>
2024-08-25 16:50:25 -0500

    updated with links and new addys for 2024

--- javascript.js
+++ javascript.js
@@ -1,19 +1,22 @@
-var blogs = [
-"https://ameyali12365.blogspot.com/", 
-"http://belahadleyosu.blogspot.com/",
-"https://azizelias339.blogspot.com/",
-"https://graciesblog76.blogspot.com/",
-"https://haneenabouhassounblogforevolution.blogspot.com",
-"https://evolutionodance.blogspot.com/",
-"https://evolutionbymarie.blogspot.com",
-"https://premedmel.blogspot.com/",
-"https://nateokstate.blogspot.com/",
-"https://evolution-em.blogspot.com"
+const blogs = [
+"https://mmeiner.blogspot.com",
+"https://knowitulover.blogspot.com",
+"https://biol4133-evolution.blogspot.com",
+"https://thoughtmutation.blogspot.com",
+"https://desscardona.blogspot.com",
+"https://jasmineplata-evolution.blogspot.com",
+"https://osu-ashe.blogspot.com",
+"https://katevolutionarybiology.blogspot.com",
+"https://acr-evolution.blogspot.com",
+"https://shortstack123.blogspot.com"
+];
 
-
-]
-
+// Function to display a new blog link
 function newBlog() {
-  var randomNumber = Math.floor(Math.random() * (blogs.length));
-  document.getElementById('blogDisplay').innerHTML = blogs[randomNumber];
+  // Select a random blog from the array
+  const randomBlog = blogs[Math.floor(Math.random() * blogs.length)];
+
+  // Display the blog as a clickable link
+  const blogDisplayDiv = document.getElementById("blogDisplay");
+  blogDisplayDiv.innerHTML = `<a href="${randomBlog}" target="_blank">${randomBlog}</a>`;
 }
